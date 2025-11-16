67a20707d2b06b9ea9634f383471cfb72e424db6
free514dom <free514dom@proton.me>
2025-11-16 16:20:10 -0500

    update

--- leetcode/20-ValidParentheses/Solution.java
+++ leetcode/20-ValidParentheses/Solution.java
@@ -2,14 +2,42 @@
  *parentheses括号
  *determine决定
  *brackets括号
- *
+ *correct正确的
+ *corresponding相应的
+ *consists由...组成
+ *character字符
  *
  *
  *
  ***/
+//s = "{}"
+import java.util.Stack;
 class Solution {
     public boolean isValid(String s) {
-        
+       Stack<Character> stack = new Stack<>();
+       for(char c: s.toCharArray()){
+           if(c==')' || c=='}' || c==']'){
+               if(stack.isEmpty()){
+                   return false;
+               }
+               char x = stack.pop();
+               if(c == ')' && x != '('){
+                   return false;
+               }
 
+               if(c == ']' && x != '['){
+                   return false;
+               }
+
+               if(c == '}' && x != '{'){
+                   return false;
+               }
+           }else{
+               stack.push(c);
+               
+           }
+       }
+    
+       return stack.isEmpty();
     }
 }
