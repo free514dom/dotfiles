f87b589b45dc9bbfeea0a819057bc3ece7203034
free514dom <free514dom@proton.me>
2025-11-16 11:34:02 -0500

    udpate

--- leetcode/1-TwoSum/Solution.java
+++ leetcode/1-TwoSum/Solution.java
@@ -1,11 +1,12 @@
 /***生词 
-*
-*
-*
-*
-*
-*
-* 
+ * indices索引
+*add up to加起来等于
+*assume假设 
+*exactly确切的
+*solution解决方案
+*order顺序,命令
+*constrains限制
+*valid有效的
 * **/
 class Solution {
     public int[] twoSum(int[] nums, int target) {
