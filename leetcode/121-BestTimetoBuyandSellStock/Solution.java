554979a3ab60f141da24d68254420c3ac4c22643
free514dom <free514dom@proton.me>
2025-11-17 17:19:00 -0500

    update

--- leetcode/121-BestTimetoBuyandSellStock/Solution.java
+++ leetcode/121-BestTimetoBuyandSellStock/Solution.java
@@ -9,6 +9,15 @@
 ***/
 class Solution {
     public int maxProfit(int[] prices) {
-        
+        int currentMaxProfit = 0;
+       // prices = {7,1,5,3,6,4};
+        for(int i=0;i<prices.length-1;i++){
+            for(int j=i+1;j<prices.length;j++){
+                if(prices[j]-prices[i]>currentMaxProfit){
+                    currentMaxProfit = prices[j]-prices[i];
+                }
+            }
+        }
+        return currentMaxProfit;
     }
 }
