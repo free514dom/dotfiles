bb64b8137c31d079cca653a6f8a78e476502c9f3
free514dom <free514dom@proton.me>
2025-11-18 14:50:48 +0800

    update

--- leetcode/1-TwoSum/Solution.java
+++ leetcode/1-TwoSum/Solution.java
@@ -13,6 +13,7 @@ class Solution {
 //        int[] nums = {2,7,11,15};
 //        int target = 9;
 //
+//
         for(int i = 0; i < nums.length-1;i++){
             for(int j = i+1;j < nums.length;j++){
                 if(nums[i]+nums[j]==target){
